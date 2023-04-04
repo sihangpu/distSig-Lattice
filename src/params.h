9777e4f4b14031b57815b60c8fcf2b367cdc494a
Sihang Pu <push.beni@gmail.com>
2023-04-04 18:19:56 +0200

    fix params

--- src/params.h
+++ src/params.h
@@ -8,49 +8,49 @@
 #define PARAM_SET 1
 
 //====== BE CAREFUL TO CHANGE THE FOLLOWING PARAMS  =======
-#define MOD_38 274810798081L
-#define MOD_39 549753716737L
-#define MOD_40 1095216660481L
+#define MOD_30 1073479681L
+#define MOD_35 34359214081L
+#define MOD_44 17592186028033L
 
-#define SIGMA_Y_30 1522429331L
-#define SIGMA_Y_31 2309154838L
-#define SIGMA_Y_32 3493703138L
+#define SIGMA_Y_24 13534249L
+#define SIGMA_Y_26 86079875L
+#define SIGMA_Y_31 2405744071L
 
 #define SIGMA_SK 4 // Discrete gaussian stdev for sk
-#define DEG 256    // Poynomial degree + 1
+#define DEG 256    // Poynomial degree + 1, should be greater than TAU
 #define ROWS 8     // Rows of matrix A
 #define COLS 7     // Columns
 #define M_REJ 1.0  // Rejection Sampling constant factor -- M
 #define TAU 23     // L1 bound of the Challenge
 
 #if (PARAM_SET == 1)
-#define MODULUS_BITS 38
-#define FIXED_MODULUS (MOD_38)
+#define MODULUS_BITS 30
+#define FIXED_MODULUS (MOD_30)
 #define L_COMMS 4            // # of Commitments: NEEDS TO BE <= 10
 #define N_USERS 2            // # of Users
-#define DELTA 31             // 2^DELTA \approx SIGMA_Y
-#define SIGMA_Y (SIGMA_Y_30) // Discrete gaussian stdev for y
+#define DELTA 24             // 2^DELTA \approx SIGMA_Y
+#define SIGMA_Y (SIGMA_Y_24) // Discrete gaussian stdev for y
 #elif (PARAM_SET == 2)
-#define MODULUS_BITS 39
-#define FIXED_MODULUS (MOD_39)
+#define MODULUS_BITS 35
+#define FIXED_MODULUS (MOD_35)
 #define L_COMMS 3            // # of Commitments: NEEDS TO BE <= 10
 #define N_USERS 3            // # of Users
-#define DELTA 31             // 2^DELTA \approx SIGMA_Y
-#define SIGMA_Y (SIGMA_Y_31) // Discrete gaussian stdev for y
+#define DELTA 26             // 2^DELTA \approx SIGMA_Y
+#define SIGMA_Y (SIGMA_Y_26) // Discrete gaussian stdev for y
 #elif (PARAM_SET == 3)
-#define MODULUS_BITS 40
-#define FIXED_MODULUS (MOD_40)
+#define MODULUS_BITS 44
+#define FIXED_MODULUS (MOD_44)
 #define L_COMMS 2            // # of Commitments: NEEDS TO BE <= 10
 #define N_USERS 5            // # of Users
-#define DELTA 32             // 2^DELTA \approx SIGMA_Y
-#define SIGMA_Y (SIGMA_Y_32) // Discrete gaussian stdev for y
+#define DELTA 31             // 2^DELTA \approx SIGMA_Y
+#define SIGMA_Y (SIGMA_Y_31) // Discrete gaussian stdev for y
 #elif (PARAM_SET == 4)
-#define MODULUS_BITS 40
-#define FIXED_MODULUS (MOD_40)
+#define MODULUS_BITS 44
+#define FIXED_MODULUS (MOD_44)
 #define L_COMMS 2            // # of Commitments: NEEDS TO BE <= 10
 #define N_USERS 7            // # of Users
-#define DELTA 32             // 2^DELTA \approx SIGMA_Y
-#define SIGMA_Y (SIGMA_Y_32) // Discrete gaussian stdev for y
+#define DELTA 31             // 2^DELTA \approx SIGMA_Y
+#define SIGMA_Y (SIGMA_Y_31) // Discrete gaussian stdev for y
 #endif
 
 static constexpr long MODULUS_BYTES = (MODULUS_BITS & 0x07) == 0 ? MODULUS_BITS >> 3 : (MODULUS_BITS >> 3) + 1;
